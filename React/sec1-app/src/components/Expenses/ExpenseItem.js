734a697541fa19840b4344f07b138a4ef142e4fd
Christopher Burton <mogul27442@gmail.com>
2023-09-04 14:00:00 +0100

    Engineer Stand-up

--- React/sec1-app/src/components/Expenses/ExpenseItem.js
+++ React/sec1-app/src/components/Expenses/ExpenseItem.js
@@ -10,10 +10,14 @@ import "./ExpenseItem.css";
 // Here props contains, date, title, amount
 // Can also create props using object destructuring from JS {}
 function ExpenseItem(props) {
+  const clickHandler = () => {
+    console.log("Clicked!!")
+  }
     // Add Javascript code to function containing HTML
     // Data is date object so can't be put in as a string
     // Use curly braces for parameters or Javascript expression
 
+    // In React we can use DOM events for any HTML element
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date} />
@@ -21,6 +25,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }

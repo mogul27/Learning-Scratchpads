907571e361112bd94b2ed5376cb2e779856af01e
Christopher Burton <mogul27442@gmail.com>
2023-09-04 16:28:22 +0100

    End of Mon 4th September

--- React/sec1-app/src/components/Expenses/ExpenseItem.js
+++ React/sec1-app/src/components/Expenses/ExpenseItem.js
@@ -1,28 +1,44 @@
 // Import another component
-import ExpenseDate from './ExpenseDate'
-import Card from '../UI/Card'
+import React, { useState } from 'react'
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
 // Make project aware of corresponding css file
 import "./ExpenseItem.css";
-
 
 // Props holds all the values (k:v pairs) that are passed in
 // Here props contains, date, title, amount
 // Can also create props using object destructuring from JS {}
 function ExpenseItem(props) {
-  const clickHandler = () => {
-    console.log("Clicked!!")
-  }
-    // Add Javascript code to function containing HTML
-    // Data is date object so can't be put in as a string
-    // Use curly braces for parameters or Javascript expression
 
-    // In React we can use DOM events for any HTML element
+  //use State returns an array that contains two elements
+  // 1. Variable itself
+  // 2. func to assigning new value to the variable
+  const [title, setTitle] = useState(props.title)
+
+  // Use state is a React hook (Begin with use)
+  // Must be called within component functions
+  // NOT nested within
+  // Use State creates a special vairable that when changed
+  // causes React to re-evaluate the component
+  // Argument is initial state
+  useState(props.title);
+
+  const clickHandler = () => {
+    // This causes react to redraw the screen
+    setTitle('Updated!');
+  };
+  // Add Javascript code to function containing HTML
+  // Data is date object so can't be put in as a string
+  // Use curly braces for parameters or Javascript expression
+
+  // In React we can use DOM events for any HTML element
   return (
     <Card className="expense-item">
-        <ExpenseDate date={props.date} />
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
